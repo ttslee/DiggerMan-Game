2355d2b886b0181a7817b66d224fe8cb28731a44
Teo Lee <ttslee@users.noreply.github.com>
2017-05-01 16:11:56 -0700

    Implemented Protester leave algorithm

--- StudentWorld.h
+++ StudentWorld.h
@@ -4,6 +4,29 @@
 #include "Actor.h"
 #include <ctime>
 // Students:  Add code to this file, StudentWorld.cpp, Actor.h, and Actor.cpp
+class RunawayBox
+{
+public:
+	RunawayBox(int xCoord, int yCoord, bool canWalk) :
+		x(xCoord), 
+		y(yCoord), 
+		m_walkFlag(canWalk), 
+		distanceFromStart(0)
+	{
+	}
+	int getX(){ return x; }
+	int getY(){ return y; }
+	bool canWalk(){ return m_walkFlag; }
+	int getDistance() { return distanceFromStart; }
+
+	void setDistance(int dis){ distanceFromStart = dis; }
+	void setWalkFlag(bool flag){ m_walkFlag = flag; }
+private:
+	int x;
+	int y;
+	bool m_walkFlag;
+	int distanceFromStart;
+};
 struct EmptySquare
 {
 	EmptySquare(int xCoord, int yCoord) : x(xCoord), y(yCoord)
@@ -36,30 +59,33 @@ public:
 	{
 		return m_diggerMan;
 	}
-	std::vector<std::shared_ptr<Protester>>			getProtesters()
-	{
-		return m_protesters;
+	std::vector<std::shared_ptr<Protester>>			getProtesters()	
+	{ 
+		return m_protesters; 
 	}
-	std::vector<std::shared_ptr<Actor>>				getActors()
+	std::vector<std::shared_ptr<Actor>>				getActors()		
 	{
-		return m_actors;
+		return m_actors; 
 	}
-	std::vector<std::vector<std::unique_ptr<Dirt>>> *getDirt()
-	{
-		return &m_dirt;
+	std::vector<std::vector<std::unique_ptr<Dirt>>> *getDirt()		
+	{ 
+		return &m_dirt; 
 	}
-
-	const int getDiggerHealth() const
+	std::vector<std::vector<std::shared_ptr<RunawayBox>>> *getRunawayGrid()
 	{
-		return m_diggerMan->getHealth();
+		return &m_runGrid;
 	}
-	const int getNumSquirtsLeft() const
+	const int getDiggerHealth() const										
 	{
-		return m_diggerMan->getNumSquirts();
+		return m_diggerMan->getHealth(); 
 	}
-	const int getTicks() const
+	const int getNumSquirtsLeft() const									
 	{
-		return m_ticks;
+		return m_diggerMan->getNumSquirts(); 
+	}
+	const int getTicks() const											
+	{
+		return m_ticks; 
 	}
 	const int getBoulder() const
 	{
@@ -73,9 +99,9 @@ public:
 	{
 		return m_oilCount;
 	}
-	void getShortestPathOut(int x, int y);
-	void decOil()
-	{
+	void updateRunGrid(int x, int y);
+	void decOil()													
+	{ 
 		m_oilCount--;
 	}
 	void setMaxProt();
@@ -85,6 +111,7 @@ public:
 	void initNumOil();
 	void initProbGoodie();
 	void initTicks();
+	void decProtCount(){ m_protesterCount--; }
 	double distance(int x1, int x2, int y1, int y2);
 
 private:
@@ -94,13 +121,14 @@ private:
 	void incTicks();
 	void initMaxProt();
 	void initProbHard();
+	void fillRunawayGrid();
 
 	std::vector<EmptySquare>						m_emptySquares;
 	std::vector<std::shared_ptr<Actor>>				m_actors;
 	std::vector<std::shared_ptr<Protester>>			m_protesters;
 	std::shared_ptr<DiggerMan>						m_diggerMan;
 	std::vector<std::vector<std::unique_ptr<Dirt>>>	m_dirt;
-
+	std::vector<std::vector<std::shared_ptr<RunawayBox>>> m_runGrid;
 	int m_oilCount;
 	int m_boulderCount;
 	int m_goldCount;
