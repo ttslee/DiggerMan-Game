8d6756943c124061c76e492a77c0c2ccf478e76a
Teo Lee <ttslee@users.noreply.github.com>
2017-05-01 16:09:53 -0700

    Implemented Protester leave algorithm

--- StudentWorld.cpp
+++ StudentWorld.cpp
@@ -1,144 +1,385 @@
-#ifndef STUDENTWORLD_H_
-#define STUDENTWORLD_H_
-#include "GameWorld.h"
-#include "Actor.h"
-#include <ctime>
-// Students:  Add code to this file, StudentWorld.cpp, Actor.h, and Actor.cpp
-class RunawayBox
+#include "StudentWorld.h"
+using namespace std;
+
+GameWorld* createStudentWorld(string assetDir)
 {
-public:
-	RunawayBox(int xCoord, int yCoord, bool canWalk) :
-		x(xCoord), 
-		y(yCoord), 
-		m_walkFlag(canWalk), 
-		distanceFromStart(0)
-	{
-	}
-	int getX(){ return x; }
-	int getY(){ return y; }
-	bool canWalk(){ return m_walkFlag; }
-	int getDistance() { return distanceFromStart; }
+	return new StudentWorld(assetDir);
+}
 
-	void setDistance(int dis){ distanceFromStart = dis; }
-	void setWalkFlag(bool flag){ m_walkFlag = flag; }
-private:
-	int x;
-	int y;
-	bool m_walkFlag;
-	int distanceFromStart;
-};
-struct EmptySquare
+int StudentWorld::init()
 {
-	EmptySquare(int xCoord, int yCoord) : x(xCoord), y(yCoord)
-	{
+	initTicks();
+	initNumBould();
+	initNumGold();
+	initNumOil();
+	initProbGoodie();
+	m_protesterCount = 0;
+	int randX = 0, randY = 0;
+	initProtTicks();
+	initMaxProt();
+	initProbHard();
+	m_diggerMan = std::make_shared<DiggerMan>(this);
+	fillDirt();
+	fillRunawayGrid();
+	initEmptySquares();
+	for (int i = 0; i < 3; i++)
+	{	// Fills StudentWorld with Goodies
+		switch (i)
+		{
+		case 0:
+			fillGoodies(randX, randY, boulder);
+			break;
+		case 1:
+			fillGoodies(randX, randY, gold);
+			break;
+		case 2:
+			fillGoodies(randX, randY, oil);
+		}
 	}
-	int x;
-	int y;
-};
-class StudentWorld : public GameWorld
+	m_protesters.emplace_back(make_shared<RegularProtester>(this));
+	//TESTING DISTANCES BETWEEN GOODIES
+	//for (size_t i = 0; i < m_actors.size(); i++)
+	//{// Nested for loop checking all actors against eachother
+	//	for (size_t j = 0; j < m_actors.size(); j++) 
+	//	{
+	//		if (i == j)
+	//			continue;
+	//		cout << distance(m_actors[i]->getX(), m_actors[j]->getX(), m_actors[i]->getY(), m_actors[j]->getY()) << endl; // Outputs distances to console. 
+	//		assert(distance(m_actors[i]->getX(), m_actors[j]->getX(), m_actors[i]->getY(), m_actors[j]->getY() > 6));
+	//	}
+	//}
+	return GWSTATUS_CONTINUE_GAME;
+}
+
+int StudentWorld::move()
 {
-public:
-	StudentWorld(std::string assetDir)
-		: GameWorld(assetDir), m_ticks(1)
+	
+	if (!m_diggerMan->isDead())
 	{
-		srand(static_cast<unsigned int>(time(nullptr)));
-	}
-	virtual int init();
-	virtual int move();
-	virtual void cleanUp();
+		//high_resolution_clock::time_point t1 = high_resolution_clock::now();
+		incTicks();
+		setGameStatText(formatGameStats());
+		if (m_oilCount == 0)
+		{
+			playSound(SOUND_FINISHED_LEVEL);
+			return GWSTATUS_FINISHED_LEVEL;
+		}
+		if (m_ticks % m_protTicks == 0 && m_protesterCount < m_maxProt)
+		{
+			int regOrhard = rand() % 100;
+			if (regOrhard < m_probHard)
+				m_protesters.emplace_back(make_shared<HardcoreProtester>(this));
+			else
+				m_protesters.emplace_back(make_shared<RegularProtester>(this));
+			m_protesterCount++;
+		}
+		if (rand() % m_probGoodie == 1)
+		{
+			if ((rand() % 5 + 1) < 4)
+			{
+				int spawn = rand() % m_emptySquares.size();
+				addActor(make_shared<Water>(this, m_emptySquares[spawn].x, m_emptySquares[spawn].y));
+			}
+			else
+				addActor(make_shared<Sonar>(this, 0, 60));
+		}
+		m_diggerMan->doSomething();
+		for (auto & a : m_actors)
+		{
+			if (!a->isDead())
+				a->doSomething();
+		}
+		for (auto & p : m_protesters)
+		{
+			if (!p->isDead())
+				p->doSomething();
+		}
+		getDiggerMan()->setSonarFlag(false);
+		/*high_resolution_clock::time_point t2 = high_resolution_clock::now();
+		auto duration = duration_cast<milliseconds>(t2 - t1).count();*/
 
-	std::string formatGameStats() const;
-	void addActor(std::shared_ptr<Actor> obj);
+		/*std::cout << duration;*/
+		return GWSTATUS_CONTINUE_GAME;
+	}
+	else
+	{
+		decLives();
+		if (getLives() == 0)
+			playSound(SOUND_PLAYER_DIE);
+		else
+			playSound(SOUND_PLAYER_GIVE_UP);
+		return GWSTATUS_PLAYER_DIED;
+	}
+	
+}
 
-	bool containsSquare(int, int);
-	std::vector<EmptySquare>						*getEmptySquares()
+void StudentWorld::cleanUp()
+{
+	m_actors.clear();
+	m_protesters.clear();
+	for (auto &a : m_dirt)
 	{
-		return &m_emptySquares;
+		a.clear();
 	}
-	std::shared_ptr<DiggerMan>						getDiggerMan()
+	m_dirt.clear();
+	for (auto & r : m_runGrid)
 	{
-		return m_diggerMan;
+		r.clear();
 	}
-	std::vector<std::shared_ptr<Protester>>			getProtesters()	
-	{ 
-		return m_protesters; 
-	}
-	std::vector<std::shared_ptr<Actor>>				getActors()		
-	{
-		return m_actors; 
-	}
-	std::vector<std::vector<std::unique_ptr<Dirt>>> *getDirt()		
-	{ 
-		return &m_dirt; 
-	}
-	std::vector<std::vector<std::shared_ptr<RunawayBox>>> *getRunawayGrid()
-	{
-		return &m_runGrid;
-	}
-	const int getDiggerHealth() const										
-	{
-		return m_diggerMan->getHealth(); 
-	}
-	const int getNumSquirtsLeft() const									
-	{
-		return m_diggerMan->getNumSquirts(); 
-	}
-	const int getTicks() const											
-	{
-		return m_ticks; 
-	}
-	const int getBoulder() const
-	{
-		return m_boulderCount;
-	}
-	const int getGold() const
-	{
-		return m_goldCount;
-	}
-	const int getOil() const
-	{
-		return m_oilCount;
-	}
-	void updateRunGrid(int x, int y);
-	void decOil()													
-	{ 
-		m_oilCount--;
-	}
-	void setMaxProt();
-	void initEmptySquares();
-	void initNumBould();
-	void initNumGold();
-	void initNumOil();
-	void initProbGoodie();
-	void initTicks();
-	void decProtCount(){ m_protesterCount--; }
-	double distance(int x1, int x2, int y1, int y2);
+	m_runGrid.clear();
+	m_emptySquares.clear();
+}
 
-private:
-	void fillGoodies(int, int, ActorType);
-	void fillDirt();
-	void initProtTicks();
-	void incTicks();
-	void initMaxProt();
-	void initProbHard();
-	void fillRunawayGrid();
+std::string StudentWorld::formatGameStats() const 
+{
+	ostringstream os;
 
-	std::vector<EmptySquare>						m_emptySquares;
-	std::vector<std::shared_ptr<Actor>>				m_actors;
-	std::vector<std::shared_ptr<Protester>>			m_protesters;
-	std::shared_ptr<DiggerMan>						m_diggerMan;
-	std::vector<std::vector<std::unique_ptr<Dirt>>>	m_dirt;
-	std::vector<std::vector<std::shared_ptr<RunawayBox>>> m_runGrid;
-	int m_oilCount;
-	int m_boulderCount;
-	int m_goldCount;
-	int	m_ticks;
-	int m_maxProt;
-	int	m_protTicks;
-	int	m_probHard;
-	int m_protesterCount;
-	int m_probGoodie;
-};
+	os << left << "Lvl: " << setw(2) << getLevel() << " Hlth: " << setw(3) << m_diggerMan->getHealth() << "% Wtr: "
+		<< setw(2) << (m_diggerMan->getNumSquirts() * 10) << " Gld: " << setw(2) << m_diggerMan->getGold()
+		<< " Sonar: " << setw(2) << m_diggerMan->getSonarCount() << " Oil Left: " << setw(2) << getOil()
+		<< " Scr: " << setfill('0') << setw(6) << getScore();
 
+	return os.str();
+}
+void StudentWorld::addActor(std::shared_ptr<Actor> obj)
+{
+	m_actors.emplace_back(obj);
+}
+void StudentWorld::fillGoodies(int randX, int randY, ActorType gdy)
+{
+	switch (gdy)
+	{
+	case boulder:
+	{
+		for (int j = 0; j < m_boulderCount; j++)
+		{
+			if (j == 0)
+			{
+				do
+				{
+					randX = rand() % 60;
+					randY = ((rand() % 37) + 20);
+				} while (randX >= 27 && randX <= 33 && randY > 0);
+				m_actors.emplace_back(std::make_shared<Boulder>(this, randX, randY));
+			}
+			else
+			{
+				do
+				{
+					randX = rand() % 60;
+					randY = ((rand() % 37) + 20);
+					if ((randX >= 27 && randX <= 33) && randY > 0)
+						continue;
+					size_t b = 0;
+					do
+					{
+						if (distance(randX, m_actors[b]->getX(), randY, m_actors[b]->getY()) > 6)
+						{
+							b++;
+							continue;
+						}
+						break;
+					} while (b < m_actors.size());
+					if (b == m_actors.size())
+					{
+						m_actors.emplace_back(std::make_shared<Boulder>(this, randX, randY));
+						break;
+					}
+				} while (true);
+			}
+		}
+		for (auto e : m_actors)
+			std::cout << "BOULDER X: " << e->getX() << "  BOULDER Y: " << e->getY() << endl;
+		break;
+	}
+	case gold:
+	case oil:
+	{
+		int count = 0;
+		if (gdy == gold)
+			count = m_goldCount;
+		else
+			count = m_oilCount;
+		for (int j = 0; j < count; j++)
+		{
+			do
+			{
+				randX = rand() % 60;
+				randY = rand() % 57;
+				if ((randX >= 27 && randX <= 33) && randY > 0)
+					continue;
+				size_t b = 0;
+				do
+				{
+					if (distance(randX, m_actors[b]->getX(), randY, m_actors[b]->getY()) > 6)
+					{
+						b++;
+						continue;
+					}
+					break;
+				} while (b < m_actors.size());
+				if (b == m_actors.size()) {
+					if (gdy == gold)
+						m_actors.emplace_back(std::make_shared<Nugget>(this, randX, randY));
+					else
+						m_actors.emplace_back(std::make_shared<Oil>(this, randX, randY));
+					break;
+				}
+			} while (true);
+		}
+		break;
+	}
+	}
+}
+void StudentWorld::fillDirt() {
+	for (int i = 0; i < 60; i++)
+	{
+		m_dirt.push_back(std::vector<std::unique_ptr<Dirt>>());
+		for (int j = 0; j < 64; j++) 
+		{
+			if ((i >= 4) && (j >= 30 && j <= 33))
+			{
+				m_dirt[i].emplace_back(std::make_unique<Dirt>(this, j, i, false, true));
+				continue;
+			}
+			m_dirt[i].emplace_back(std::make_unique<Dirt>(this, j, i));
+		}
+	}
+}
+void StudentWorld::fillRunawayGrid()
+{
+	for (int i = 0; i < 61; i++)
+	{
+		m_runGrid.push_back(std::vector<std::shared_ptr<RunawayBox>>());
+		for (int j = 0; j < 61; j++)
+		{
+			if (i == 60)
+				m_runGrid[i].emplace_back(std::make_shared<RunawayBox>(j, i, true));
+			else if (i >= 4 && j == 30)
+				m_runGrid[i].emplace_back(std::make_shared<RunawayBox>(j, i, true));
+			else
+				m_runGrid[i].emplace_back(std::make_shared<RunawayBox>(j, i, false));
+		}
+	}
+}
+void StudentWorld::updateRunGrid(int x, int y)
+{
+	high_resolution_clock::time_point t1 = high_resolution_clock::now();
+	queue<std::shared_ptr<RunawayBox>> run_q;
+	run_q.push(m_runGrid[60][60]);
+	while (!run_q.empty())
+	{
+		std::shared_ptr<RunawayBox> front = run_q.front();
+		run_q.pop();
+		// up
+		if (front->getY() < 60 && m_runGrid[front->getY() + 1][front->getX()]->canWalk())
+		{
+			if (m_runGrid[front->getY() + 1][front->getX()]->getDistance() >= front->getDistance() || m_runGrid[front->getY() + 1][front->getX()]->getDistance() == 0)
+			{
+				m_runGrid[front->getY() + 1][front->getX()]->setDistance(front->getDistance() + 1);
+				run_q.push(m_runGrid[front->getY() + 1][front->getX()]);
+			}
+			if (front->getX() == x && front->getY() + 1 == y)
+				break;
+		}
+		// left
+		if (front->getX() > 0 && m_runGrid[front->getY()][front->getX() - 1]->canWalk())
+		{
+			if (m_runGrid[front->getY()][front->getX() - 1]->getDistance() >= front->getDistance() || m_runGrid[front->getY()][front->getX() - 1]->getDistance() == 0)
+			{
+				m_runGrid[front->getY()][front->getX() - 1]->setDistance(front->getDistance() + 1);
+				run_q.push(m_runGrid[front->getY()][front->getX() - 1]);
+			}
+			if (front->getX() - 1 == x && front->getY() == y)
+				break;
+		}
+		// right
+		if (front->getX() < 60 && m_runGrid[front->getY()][front->getX() + 1]->canWalk())
+		{
+			if (m_runGrid[front->getY()][front->getX() + 1]->getDistance() >= front->getDistance() || m_runGrid[front->getY()][front->getX() + 1]->getDistance() == 0)
+			{
+				m_runGrid[front->getY()][front->getX() + 1]->setDistance(front->getDistance() + 1);
+				run_q.push(m_runGrid[front->getY()][front->getX() + 1]);
+			}
+			if (front->getX() + 1 == x && front->getY() == y)
+				break;
+		}
+		// down
+		if (front->getY() > 0 && m_runGrid[front->getY() - 1][front->getX()]->canWalk())
+		{
+			if (m_runGrid[front->getY() - 1][front->getX()]->getDistance() >= front->getDistance() || m_runGrid[front->getY() - 1][front->getX()]->getDistance() == 0)
+			{
+				m_runGrid[front->getY() - 1][front->getX()]->setDistance(front->getDistance() + 1);
+				run_q.push(m_runGrid[front->getY() - 1][front->getX()]);
+			}
+			if (front->getX() == x && front->getY() - 1 == y)
+				break;
+		}
+	}
+	m_runGrid[60][60]->setDistance(0); 
+	while (!run_q.empty())
+		run_q.pop();
+	high_resolution_clock::time_point t2 = high_resolution_clock::now();
+	auto duration = duration_cast<milliseconds>(t2 - t1).count();
+	std::cout << duration;
+}
+bool StudentWorld::containsSquare(int xCoord, int yCoord)
+{
+	for (auto s : m_emptySquares)
+	{
+		if (s.x == xCoord && s.y == yCoord)
+			return true;
+	}
+	return false;
+}
+void StudentWorld::initEmptySquares()
+{
+	for (int i = 4; i <= 56; i++)
+	{
+		m_emptySquares.emplace_back(30, i);
+	}
+}
 
-#endif // STUDENTWORLD_H_
+void StudentWorld::initProtTicks()
+{
+	m_protTicks = max(25, (200 - static_cast<int>(getLevel())));
+}
+void StudentWorld::initMaxProt()
+{
+	m_maxProt = min(15, static_cast<int>(2 + getLevel() * 1.5));
+}
+void StudentWorld::initProbHard()
+{
+	m_probHard = min(90, static_cast<int>(getLevel()) * 10 + 30);
+}
+
+void StudentWorld::initNumBould()
+{
+	m_boulderCount = min(static_cast<int>(getLevel()) / 2 + 2, 7);
+}
+void StudentWorld::initNumGold()
+{
+	m_goldCount = max(5 - static_cast<int>(getLevel()) / 2, 2);
+}
+void StudentWorld::initNumOil()
+{
+	m_oilCount = min(2 + static_cast<int>(getLevel()), 18);
+}
+void StudentWorld::initProbGoodie()
+{
+	m_probGoodie = getLevel() * 25 + 300;
+}
+void StudentWorld::incTicks() 
+{
+	m_ticks++;
+}
+void StudentWorld::initTicks()
+{
+	m_ticks = 0;
+}
+double StudentWorld::distance(int x1, int x2, int y1, int y2)
+{
+	return sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+}
+
+// Students:  Add code to this file (if you wish), StudentWorld.h, Actor.h and Actor.cpp
